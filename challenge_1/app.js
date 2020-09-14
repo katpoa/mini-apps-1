cc7820ef90b23c55e2a1aed8d3b51593b5f063d8
Katie Poa <kpoa@berkeley.edu>
2020-09-14 14:29:53 -0700

    Moons and Stars galore, player turn displaying properly

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,31 +1,51 @@
 document.addEventListener('DOMContentLoaded', () => {
-
   // the first move always starts with player X
   const squares = document.querySelectorAll('.table td');
   console.log(squares);
   const showPlayer = document.querySelector('#player');
+  let currentPlayer = 'player X';
+  showPlayer.innerHTML = currentPlayer;
 
-  let currentPlayer = 'playerX';
-  squares.forEach(square => {
-    square.addEventListener('click', clickHandler)
-  })
 
-  var clickHandler = (e) => {
+  //***** Player moves!!!
+  var clickHandler = (e, clicked = false) => {
     console.log('Square clicked!');
-    var td = e.target;
+    // document.querySelector(e.target).innerHTML = "X";
+    // showPlayer.innerHTML = currentPlayer;
+
+    const squareArray = Array.from(squares);
     const index = squareArray.indexOf(e.target);
-    showPlayer.innerHTML = currentPlayer;
-    if (currentPlayer === 'player X') {
+    if (showPlayer.innerHTML === 'player X') {
       squares[index].classList.add('playerX');
-      currentPlayer = 'player O';
+      showPlayer.innerHTML = 'player O';
     } else {
       squares[index].classList.add('playerO');
-      currentPlayer = 'player X';
+      showPlayer.innerHTML = 'player X';
     }
-  };
+    // checkWinner();
+  }
+
+  squares.forEach(square => {
+    square.addEventListener('click', clickHandler);
+    console.log('Should have event listener now: ', square);
+  });
 
   // the app detects a win or tie and displays an appropriate message
+  var checkWinner = (player) => {
+    //check if three in a row on any row/column/diagonal
+    var win = false;
+    var row1 = document.querySelector('#1');
+    var row2 = document.querySelector('#2');
+    var row3 = document.querySelector('#3');
 
+
+  };
+
+  var checkTie = () => {
+    //if all squares.classList !== empty
+    //!checkWinner('playerX') && !checkWinner('playerO')
+    // return "Tie! You're both winners!"
+  }
 
   // a button resets the game for a new round of gameplay
   const startOverHandler = (e) => {
