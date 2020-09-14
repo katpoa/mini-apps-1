2069155efc52b5becdfc27b6491d7f0fc36fd322
Katie Poa <kpoa@berkeley.edu>
2020-09-14 11:53:24 -0700

    Set up 3x3 table with headers and cells, add event listeners to each

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // the first move always starts with player X
+  const squares = document.querySelectorAll('.table td');
+  console.log(squares);
+  const showPlayer = document.querySelector('#player');
+
+  let currentPlayer = 'playerX';
+  squares.forEach(square => {
+    square.addEventListener('click', clickHandler)
+  })
+
+  var clickHandler = (e) => {
+    console.log('Square clicked!');
+    var td = e.target;
+    const index = squareArray.indexOf(e.target);
+    showPlayer.innerHTML = currentPlayer;
+    if (currentPlayer === 'player X') {
+      squares[index].classList.add('playerX');
+      currentPlayer = 'player O';
+    } else {
+      squares[index].classList.add('playerO');
+      currentPlayer = 'player X';
+    }
+  };
+
+  // the app detects a win or tie and displays an appropriate message
 
 
+  // a button resets the game for a new round of gameplay
+  const startOverHandler = (e) => {
+    e.preventDefault();
+  }
+
+})
