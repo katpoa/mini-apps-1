b88c13ec1d1cd94a2d32472cf5db65a3e6ada751
Katie Poa <kpoa@berkeley.edu>
2020-09-14 20:50:57 -0700

    Finish checkWinner including checkDiagonals, define addClickHandlers to be called upon Start Over and add a removeClickHandlers for when a player wins

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,16 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   // the first move always starts with player X
   const squares = document.querySelectorAll('.table td');
-  console.log(squares);
   const showPlayer = document.querySelector('#player');
   var currentPlayer = 'Starlight';
   showPlayer.innerHTML = currentPlayer;
   const end = document.querySelector('#end');
   end.innerHTML = 'Play on!';
-
+// ----------------------------------------------
   //***** PLAYER MOVES
   var clickHandler = (e, clicked = false) => {
-    console.log('Square clicked!');
     const squareArray = Array.from(squares);
     const index = squareArray.indexOf(e.target);
 
@@ -28,20 +26,31 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       }
     }
-    if (checkWinner('Starlight') || checkWinner('Moonlight')) {
-      if (checkWinner('Starlight')) {
+    if (checkWinner('Starlight', index) || checkWinner('Moonlight', index)) {
+      if (checkWinner('Starlight', index)) {
         end.innerHTML = 'Game Over! Starlight wins!';
+        removeClickHandlers(clickHandler);
       } else {
         end.innerHTML = 'Game Over! Moonlight wins!';
+        removeClickHandlers(clickHandler);
       }
     } else if (boardFilled() && !checkWinner('Starlight', index) && !checkWinner('Moonlight', index)) {
       end.innerHTML = 'Draw! You\'re both winners!';
     }
   }
   //Add clickHandler to each square
-  squares.forEach(square => {
-    square.addEventListener('click', clickHandler);
-  });
+  var addClickHandlers = (clickHandler) => {
+    squares.forEach(square => {
+      square.addEventListener('click', clickHandler);
+    });
+  }
+  addClickHandlers(clickHandler);
+  //Remove clickHandlers from each square (after someone wins!)
+  var removeClickHandlers = (clickHandler) => {
+    squares.forEach(square => {
+      square.removeEventListener('click', clickHandler);
+    });
+  }
 
   // ***** WIN/TIE condition: the app detects a win or tie and displays an appropriate message
   var boardFilled = () => {
@@ -53,7 +62,7 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     return filled;
   }
-
+// -----------------------------------------------------
   var checkWinner = (player, index) => {
     //check if three in a row on any row/column/diagonal
     var row1 = document.querySelectorAll('.table #row1');
@@ -62,7 +71,6 @@ document.addEventListener('DOMContentLoaded', () => {
     var checkRow = (row) => {
       let count = 0;
       let cells = Array.from(row[0].cells);
-      console.log(cells);
       cells.forEach(square => {
         if (square.classList.length > 1 && square.classList[1] === player) {
           count++;
@@ -88,18 +96,33 @@ document.addEventListener('DOMContentLoaded', () => {
       }
       return false;
     };
+
+    //*****Check Diagonals */
     var checkDiagonals = (index) => {
       //only if corner or middle
       //check top-right to bottom-left diagonal
-      var win1 = document.querySelector('#row1 .three').classList.length > 1;
-      var win12 = document.querySelector('#row3 .one').classList.length > 1
-      var win13 = document.querySelector('#row1 .three').classList[1] === document.querySelector('#row2 .two').classList[1] === document.querySelector('#row1 .three').classList[1] === player;
-      //check top-left to bottom-right diagonal
-      var win2 = document.querySelector('#row1 .one').classList.length > 1;
-      var win21 = document.querySelector('#row3 .three').classList.length > 1;
-      var win22 = document.querySelector('#row1 .one').classList[1] === document.querySelector('#row2 .two').classList[1] === document.querySelector('#row3 .three').classList[1] === player;
-      if(document.querySelector('#row2 .two').classList.length > 1 && ((win1 && win12 && win13) || (win2 && win21 && win22))) {
-        return true;
+      if (squares[index] == document.querySelector('#row1 .three') || squares[index] == document.querySelector('#row3 .one') || squares[index] == document.querySelector('#row2 .two')) {
+
+        var win1 = document.querySelector('#row1 .three').classList.length > 1;
+        console.log('top right', win1);
+        var win12 = document.querySelector('#row3 .one').classList.length > 1
+        console.log('bottom left', win12);
+        var win13 = document.querySelector('#row1 .three').classList.contains(player) === document.querySelector('#row2 .two').classList.contains(player) === document.querySelector('#row3 .one').classList.contains(player);
+        console.log('topright to bottomleft', win13);
+        if(win1 && win12 && win13) {
+          return true;
+        }
+      } else if (squares[index] == document.querySelector('#row1 .one') || squares[index] == document.querySelector('#row3 .three') || squares[index] == document.querySelector('#row2 .two')) {
+        //check top-left to bottom-right diagonal
+        var win2 = document.querySelector('#row1 .one').classList.length > 1;
+        console.log('top left', win2);
+        var win21 = document.querySelector('#row3 .three').classList.length > 1;
+        console.log('bottom right', win21);
+        var win22 = document.querySelector('#row1 .one').classList.contains(player) === document.querySelector('#row2 .two').classList.contains(player) === document.querySelector('#row3 .three').classList.contains(player);
+        console.log('topleft to bottomright', win22);
+        if(win2 && win21 && win22) {
+          return true;
+        }
       }
       return false;
     };
@@ -109,7 +132,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     return false;
   };
-
+// -----------------------------------------------------
+//*****Start Over button - clear board */
   var startOver = document.getElementById('btn');
   // a button resets the game for a new round of gameplay
   const startOverHandler = (e) => {
@@ -126,10 +150,8 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       }
     });
-    var newDiv = document.createElement("div");
-    newDiv.innerHTML = "<span></span>";
-    end.appendChild(newDiv);
+    addClickHandlers(clickHandler);
   };
   startOver.addEventListener('click', startOverHandler);
 
-})
+});
